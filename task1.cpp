32af95e2f32ecbb3d5bae0111b810101a20c4759
MuqadasAkhtar7 <akhtarmuqadas7@gmail.com>
2025-05-29 11:52:28 +0500

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -1,5 +1,8 @@
-#include<iostream>
-int main(){
-    std::cout <<"Hello World!";
-    return 0;
+#include <iostream>  // Include the input/output stream library
+
+int main() {
+    // Print Hello World to the console
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;  // Return 0 to indicate successful execution
 }
